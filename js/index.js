b39f764451860e67e4affe4d8925552e79838468
Gabriel <gabriel.mgran20@hotmail.com>
2023-08-10 17:25:04 -0300

    Update
    Ativando sons e correção de erros.

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@
 // sons que serão usados ao clicar no botão.
 
 const sons = {
-    'A': 'Boom.wav',
-    'S': 'clap.wav',
-    'D': 'hithat.wav',
-    'F': 'kick.wav',
-    'G': 'openhat.wav',
-    'H': 'ride.wav',
-    'J': 'snare.wav',
-    'K': 'tink.wav',
-    'L': 'tom.wav',
+    'A': '04-DRUM-KIT_sounds_Boom.wav',
+    'S': '04-DRUM-KIT_sounds_clap.wav',
+    'D': '04-DRUM-KIT_sounds_hihat.wav',
+    'F': '04-DRUM-KIT_sounds_kick.wav',
+    'G': '04-DRUM-KIT_sounds_openhat.wav',
+    'H': '04-DRUM-KIT_sounds_ride.wav',
+    'J': '04-DRUM-KIT_sounds_snare.wav',
+    'K': '04-DRUM-KIT_sounds_tink.wav',
+    'L': '04-DRUM-KIT_sounds_tom.wav',
 }
 
 
@@ -32,5 +32,29 @@ const exibir = (sons) => {
 
 }
 
-exibir(sons);
+ // Som que irá tocar ao pressionar o botão
+const tocarSom = (letra) => {
+    const audio = new Audio(`./sounds/${sons[letra]}`);
+    audio.play();
 
+
+}
+
+const ativarDiv = (event) => {
+    const letra = event.target.id;
+    const letraPermitida = sons.hasOwnProperty(letra) // se falso, se não clicar em um container não vai tocar a musica, se verdadeiro tocará.
+    if(letraPermitida) {
+
+        tocarSom(letra); // tocará o som da letra que eu clicar.
+
+    }
+
+    
+    
+
+
+}
+
+exibir(sons); 
+
+document.getElementById(`container`).addEventListener(`click`, ativarDiv); // Ao clicar no botão vai ativar a div.
